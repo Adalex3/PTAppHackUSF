fc3fef597bcbdaa1930df5e7cc4b7f41aeae10bc
Bubblie01 <manrohesh@gmail.com>
2025-04-05 16:55:21 -0400

    moved the opencv stuff to the flask folder, reinstalled with pip so it matches environment, connected it with flask

--- flask/server.py
+++ flask/server.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
+from flask import Flask, render_template, Response
+from start import generate_frames
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, Flask!'
+
+@app.route('/video')
+def load_video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
