599e38dd2a0d091d73dc8954cf327552b7635c1d
Adalex3 <adalex3@icloud.com>
2025-04-05 12:48:20 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Navigate to Flask folder and run Flask server
+cd flask
+echo "Starting Flask server..."
+python3 server.py &
+FLASK_PID=$!
+cd ..
+
+# Navigate to React app and start it
+cd react/pt-app
+echo "Starting React app..."
+npm start &
+REACT_PID=$!
+cd ../..
+
+# Trap Ctrl+C and kill background processes
+trap "kill $FLASK_PID $REACT_PID" EXIT
+
+# Wait for both processes to complete
+wait
