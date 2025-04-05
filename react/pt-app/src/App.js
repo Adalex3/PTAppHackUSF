c7682992825bd9b49e67d76bb99f7c1a86aecf85
Adalex3 <adalex3@icloud.com>
2025-04-05 13:47:10 -0400

    Added basic frontend & logo

--- react/pt-app/src/App.js
+++ react/pt-app/src/App.js
@@ -5,20 +5,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo"></img>
+        <div>
+          <h1>FixurPostur</h1>
+          <h4>The all-in-one PT aid.</h4>
+          <p className="desc">Get real-time feedback on your form using your camera and AI. Follow a custom plan from your doctor or generate one instantly. Track progress, fix mistakes, and move better... smarter!</p>
+        </div>
       </header>
+
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
