846e33eebf38f2d0e6eabd11091133e45fe72dba
Bubblie01 <manrohesh@gmail.com>
2025-04-05 16:20:39 -0400

    basic body detection, will have to add more

--- mediapipe/start.py
+++ mediapipe/start.py
@@ -1,62 +1,58 @@
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
-import numpy as np
-import cv2
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+import cv2 as cv2
+import mediapipe.python.solutions.hands as mp_hands
+import mediapipe.python.solutions.drawing_utils as drawing
+import mediapipe.python.solutions.drawing_styles as drawing_styles
+import mediapipe.python.solutions.holistic as mp_holistic
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-  pose_landmarks_list = detection_result.pose_landmarks
-  annotated_image = np.copy(rgb_image)
-  
-  # Loop through the detected poses to visualize.
-  for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
+cap = cv2.VideoCapture(1)
 
-    # Draw the pose landmarks.
-    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-    pose_landmarks_proto.landmark.extend([
-      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
-    ])
-    solutions.drawing_utils.draw_landmarks(
-      annotated_image,
-      pose_landmarks_proto,
-      solutions.pose.POSE_CONNECTIONS,
-      solutions.drawing_styles.get_default_pose_landmarks_style())
-  return annotated_image
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=0) as holistic:
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-window_name = "test"
-img = cv2.imread("image.jpg")
-cv2.imshow(window_name, img)
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-# STEP 2: Create an PoseLandmarker object.
-base_options = python.BaseOptions(model_asset_path='pose_landmarker.task')
-options = vision.PoseLandmarkerOptions(
-    base_options=base_options,
-    output_segmentation_masks=True)
-detector = vision.PoseLandmarker.create_from_options(options)
+        results = holistic.process(img)
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        drawing.draw_landmarks(
+            img,
+            results.face_landmarks,
+            mp_holistic.FACEMESH_CONTOURS,
+            drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+            drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+        )
 
 
-# STEP 3: Load the input image.
-rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
+        drawing.draw_landmarks(
+            img,
+            results.right_hand_landmarks,
+            mp_holistic.HAND_CONNECTIONS,
+            drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+            drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+        )
 
-# STEP 4: Detect pose landmarks from the input image.
-detection_result = detector.detect(image)
+        drawing.draw_landmarks(
+            img,
+            results.left_hand_landmarks,
+            mp_holistic.HAND_CONNECTIONS,
+            drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+            drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+        )
 
-# STEP 5: Process the detection result. In this case, visualize it.
-annotated_rgb = draw_landmarks_on_image(rgb_image, detection_result)
-annotated_image_bgr = cv2.cvtColor(annotated_rgb, cv2.COLOR_RGB2BGR)
-cv2.imshow(window_name, annotated_image_bgr)
+        drawing.draw_landmarks(
+            img,
+            results.pose_landmarks,
+            mp_holistic.POSE_CONNECTIONS,
+            drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+            drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+        )
 
-segmentation_mask = detection_result.segmentation_masks[0].numpy_view()
-visualized_mask = np.repeat(segmentation_mask[:, :, np.newaxis], 3, axis=2) * 255
+        cv2.imshow("Full Body Sample", img)
 
-cv2.imshow(window_name, visualized_mask)
-# waits for user to press any key
-# (this is necessary to avoid Python kernel form crashing)
-cv2.waitKey(0)
+        if cv2.waitKey(27) == ord('\x1b'):
+            break
 
-# closing all open windows
+cap.release()
 cv2.destroyAllWindows()
