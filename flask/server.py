b722770eb4d74a26a308dc0c7d96dcda09088bdd
Adalex3 <adalex3@icloud.com>
2025-04-05 18:17:37 -0400

    FIXED ISSUES!!!!!!!!!!!!!!

--- flask/server.py
+++ flask/server.py
@@ -11,7 +11,7 @@ def home():
 def load_video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('is_in_frame')
+@app.route('/is_in_frame')
 def is_in_frame():
     # Checks whether all joints are visible in the frame (if parts of their body are obscured or not). Returns true or false.
     return jsonify({'in_frame': False}) # Default
@@ -24,7 +24,7 @@ def set_pose():
     # Tells the python backend what pose the user is attempting to do, as this is received from frontend
     return jsonify({'status': 'error'}) # Default return
 
-@app.route('pose_feedback')
+@app.route('/pose_feedback')
 def pose_data():
     # Returns a big JSON with lots of data about the user's movements. Has all of the pose data that the backend gets to be processed by frontend
     # This should return the position and angle and other info about every joint, for the frontend to process
